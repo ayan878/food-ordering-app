a39abea5a68163dd465a6c29aff0282ffd7e7569
ayan878 <ayanraza197@gmail.com>
2024-04-02 17:08:57 +0530

    created header ,footer and homepage

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import HomePage from "./pages/HomePage";
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout >HOME Page</Layout>} />
+        <Route path="/" element={<Layout><HomePage/></Layout>} />
       </Routes>
     </Router>
   );
