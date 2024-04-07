cb42743d56537543b8b07ffb46dfc4bbcba0b2a3
ayan878 <ayanraza197@gmail.com>
2024-04-07 23:24:03 +0530

    creating user

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,22 @@ import ReactDOM from "react-dom/client";
 import "./global.css";
 import AppRoutes from "./AppRoutes";
 import Auth0ProviderWithNavigate from "./auth/AuthProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Auth0ProviderWithNavigate>
-      <AppRoutes />
-    </Auth0ProviderWithNavigate>
+    <QueryClientProvider client={queryClient}>
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
+    </QueryClientProvider>
   </React.StrictMode>
 );
