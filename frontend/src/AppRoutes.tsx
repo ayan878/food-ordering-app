d628836234979aac73cd138223827c1dc30af07d
ayan878 <ayanraza197@gmail.com>
2024-04-12 21:39:04 +0530

    route

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,37 +1,58 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Layout from "./layouts/Layout";
-// import HomePage from "./pages/HomePage";
-
-// export default function AppRoutes() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Layout><HomePage/></Layout>} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-import HomePage from "@/pages/HomePage";
-
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 import Layout from "./layouts/Layout";
-import Hero from "./components/Hero";
 
-function AppRoutes() {
-  const router = createBrowserRouter([
-    {
-      element: <Layout />,
-      path:"/",
-      children: [
-        {
-          path: "/",
-          element: <HomePage />,
-          children:[{element:<Hero/>}]
-        },
-      ],
-    },
-  ]);
-  return <RouterProvider router={router}></RouterProvider>;
-}
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <HomePage/>
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
 
 export default AppRoutes;
+
+
+// import HomePage from "@/pages/HomePage";
+
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import Layout from "./layouts/Layout";
+// import Hero from "./components/Hero";
+// import AuthCallbackPage from "./pages/AuthCallbackPage";
+
+// function AppRoutes() {
+//   const router = createBrowserRouter([
+//     {
+//       element: <Layout />,
+//       // path:"/",
+//       children: [
+//         {
+//           path: "/",
+//           element: <HomePage />,
+//           children: [{ element: <Hero /> }],
+//         },
+//         { path: "*", element: <HomePage /> },
+//         {
+//           path: "/auth-callback",
+//           element: <AuthCallbackPage />,
+//         },
+//       ],
+//     },
+//   ]);
+//   return <RouterProvider router={router}></RouterProvider>;
+// }
+
+// export default AppRoutes;
