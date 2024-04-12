cb2e5cdf9143b388d72c243c565c9aa184313cfc
ayan878 <ayanraza197@gmail.com>
2024-04-12 21:39:48 +0530

    layout

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,40 +1,40 @@
-// import Footer from "@/components/Footer";
-// import Header from "@/components/Header";
-// import Hero from "@/components/Hero";
-// import React from "react";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
-// type Props = {
-//   children: React.ReactNode;
-// };
+type Props = {
+  children: React.ReactNode;
+  showHero?: boolean;
+};
 
-// const Layout = ({ children }: Props) => {
-//   return (
-//     <div className="flex flex-col min-h screen">
-//       <Header />
-//       <Hero/>
-//       <div className="container mx-auto flex-1 py-10"> {children}</div>
-//       <Footer/>
-//     </div>
-//   );
-// };
-
-// export default Layout;
-
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-// import Hero from '@/components/Hero';
-
-import { Outlet } from 'react-router-dom'
-
-function Layout() {
+const Layout = ({ children, showHero = false }: Props) => {
   return (
-    <div className="flex flex-col min-h screen">
+    <div className="flex flex-col min-h-screen">
       <Header />
-      {/* <Hero/> */}
-      <Outlet />
+      {showHero && <Hero />}
+      <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer />
     </div>
   );
-}
+};
 
-export default Layout
+export default Layout;
+
+// import Footer from '@/components/Footer'
+// import Header from '@/components/Header'
+// // import Hero from '@/components/Hero';
+
+// import { Outlet } from 'react-router-dom'
+
+// function Layout() {
+//   return (
+//     <div className="flex flex-col min-h screen">
+//       <Header />
+//       {/* <Hero/> */}
+//       <Outlet />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default Layout
