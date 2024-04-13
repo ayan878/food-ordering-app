5638e1d53b6f0b67bbb3f8598003a6f50b64bcd6
ayan878 <ayanraza197@gmail.com>
2024-04-13 16:09:47 +0530

    fix route error

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -24,35 +24,3 @@ const AppRoutes = () => {
 };
 
 export default AppRoutes;
-
-
-// import HomePage from "@/pages/HomePage";
-
-// import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// import Layout from "./layouts/Layout";
-// import Hero from "./components/Hero";
-// import AuthCallbackPage from "./pages/AuthCallbackPage";
-
-// function AppRoutes() {
-//   const router = createBrowserRouter([
-//     {
-//       element: <Layout />,
-//       // path:"/",
-//       children: [
-//         {
-//           path: "/",
-//           element: <HomePage />,
-//           children: [{ element: <Hero /> }],
-//         },
-//         { path: "*", element: <HomePage /> },
-//         {
-//           path: "/auth-callback",
-//           element: <AuthCallbackPage />,
-//         },
-//       ],
-//     },
-//   ]);
-//   return <RouterProvider router={router}></RouterProvider>;
-// }
-
-// export default AppRoutes;
