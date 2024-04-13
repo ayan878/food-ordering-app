058e3ea58dd06c1cb96873c92af0cc2f07f0c155
ayan878 <ayanraza197@gmail.com>
2024-04-13 16:10:04 +0530

    fix route error

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -19,22 +19,3 @@ const Layout = ({ children, showHero = false }: Props) => {
 };
 
 export default Layout;
-
-// import Footer from '@/components/Footer'
-// import Header from '@/components/Header'
-// // import Hero from '@/components/Hero';
-
-// import { Outlet } from 'react-router-dom'
-
-// function Layout() {
-//   return (
-//     <div className="flex flex-col min-h screen">
-//       <Header />
-//       {/* <Hero/> */}
-//       <Outlet />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default Layout
