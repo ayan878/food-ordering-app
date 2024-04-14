cc4f4ebf04621031bd12fd05c8aaab062e2c8fc8
ayan878 <ayanraza197@gmail.com>
2024-04-14 13:36:02 +0530

    update User profile API

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import MyUserController from "../controller/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
+
 
 
 const router = Router();
 
 // Route for creating a user
 router.post("/",jwtCheck,MyUserController.createCurrentUser);
+router.put("/",jwtCheck,jwtParse,validateMyUserRequest, MyUserController.updateCurrentUser)
 
 export default router;
